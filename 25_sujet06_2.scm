6b69e28c6095b8cbe865ba0f38fc82af7d437f29
David COBAC <david.cobac@ac-nantes.fr>
2025-07-08 17:35:25 +0200

    25-S6-2

---
+++ 25_sujet06_2.scm
@@ -0,0 +1,21 @@
+; david cobac
+; juillet 2025
+(define (dico lettre) (+ 1 (- (char->integer lettre) (char->integer #\A))))
+
+(define (codes-parfait s)
+  (let ((L (string-length s)))
+    (let boucle ((somme 0)
+		 (concat "")
+		 (indice 0))
+      (if (= indice L)
+	  (list somme
+		(string->number concat)
+		(= 0 (modulo (string->number concat) somme)))
+	  (let ((codelettre (dico (string-ref s indice))))
+	    (boucle (+ somme codelettre)
+		  (string-append concat (string codelettre))
+		  (+ indice 1)))))))
+
+; tests
+(let ((liste '("PAUL" "ALAIN")))
+  (newline)(write (map codes-parfait liste)))
