aca8402d16cb7f1b2db5c76f3bb129fb5ec06e5e
David COBAC <david.cobac@ac-nantes.fr>
2025-07-08 12:17:33 +0200

    25-S4-2 improvements

--- 25_sujet04_2.scm
+++ 25_sujet04_2.scm
@@ -1,24 +1,20 @@
 ; david cobac
 ; juillet 2025
-(define (une-etape lst)
-  (let ((N (- (length lst) 1)))
-    (let boucle ((liste lst)
-		 (N n))
-      (if (= n 0) liste
-	  (if (< (car liste) (cadr liste))
-	      (cons (car liste)
-		    (boucle (cdr liste) (- n 1)))
-	      (cons (cadr liste)
-		    (boucle (cons (car liste) (cddr liste)) (- n 1))))))))
+(define (une-etape lst nb-etapes)
+  (let boucle ((liste lst))
+    (if (= (length liste) (- (length lst) nb-etapes)) liste
+	(if (< (car liste) (cadr liste))
+	    (cons (car liste) (boucle (cdr liste)))
+	    (cons (cadr liste) (boucle (cons (car liste) (cddr liste))))))))
 
 (define (tri-bulles lst)
   (let boucle1 ((liste lst)
 		(n (- (length lst) 1)))
     (if (= n 0) liste
-	(boucle1  (une-etape liste) (- n 1)))))
+	(boucle1 (une-etape liste n) (- n 1)))))
 
 ; tests
 (let ((listes '((9 3 7 2 3 1 6) (9 7 4 3))))
       (newline)(write (map tri-bulles listes)))
       
-    
+(une-etape '(9 3 7 2 3 1 6) 6)
