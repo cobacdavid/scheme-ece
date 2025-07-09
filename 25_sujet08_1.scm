0fa0bb1024ff1445cbe6394bbd363afb90acf865
David COBAC <david.cobac@ac-nantes.fr>
2025-07-09 16:49:48 +0200

    25-S8-1

---
+++ 25_sujet08_1.scm
@@ -0,0 +1,11 @@
+; david cobac
+; juillet 2025
+(define (maximum-tableau lst)
+  (let boucle ((liste lst)
+	       (max (car lst)))
+    (if (null? liste) max
+	(boucle (cdr liste) (if (> (car liste) max) (car liste) max)))))
+
+; tests
+(let ((listes '((98 12 104 23 131 9) (-27 24 -3 15))))
+  (newline)(write (map maximum-tableau listes)))
