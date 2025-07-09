d9b491805141e3d6073912d4e5cd2a6dddf62697
David COBAC <david.cobac@ac-nantes.fr>
2025-07-09 16:32:04 +0200

    25-S7-2

---
+++ 25_sujet07_2.scm
@@ -0,0 +1,13 @@
+; david cobac
+; juillet 2025
+(define (fusion t1 t2)
+  (cond ((null? t1) t2)
+	((null? t2) t1)
+	(#t (if (< (car t1) (car t2))
+		(cons (car t1) (fusion (cdr t1) t2))
+		(cons (car t2) (fusion t1 (cdr t2)))))))
+
+; tests
+(let ((listes (list '((1 2 3) () (1 6 10) )
+		    '(() () (0 7 8 9)))))
+  (newline)(write (apply map fusion listes)))
