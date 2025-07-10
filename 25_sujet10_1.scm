30d0a6426cfec6569fe4e4396602d2c1bdc3e629
David COBAC <david.cobac@ac-nantes.fr>
2025-07-10 10:42:33 +0200

    25-S10-1 + 25-S9-2 fix binary search comparison

--- 25_sujet10_1.scm
+++ 25_sujet10_1.scm
@@ -1,22 +1,16 @@
 ; david cobac
 ; juillet 25
-
-;; procédure builtin pour faire cette recherche
-;; (define (dichotomie v x)
-;;   (= x (vector-binary-search v < (lambda (n) n) x)))
-
 (define (dichotomie v x)
   (let ((L (- (vector-length v) 1)))
     (let boucle ((debut 0)
 		 (fin L))
       (let* ((m (floor (/ (+ debut fin) 2)))
 	     (elt (vector-ref v m)))
-	(cond ((= elt x) #t)
+	(cond ((= elt x) m)
 	      ((>= debut fin) #f)
 	      ((< elt x) (boucle (+ 1 m) fin))
 	      (else (boucle debut (- m 1))))))))
 
 ; tests
-(define vect #(15 16 18 19 23 24 28 29 31 33))
-(let ((liste '(28 27)))
- (newline)(write (map (lambda (n) (dichotomie vect n)) liste)))
+(let ((vecteurs '(#(2 3 4 5 6) #(2 3 4 6 7))))
+  (newline)(write (map (lambda (v) (dichotomie v 5)) vecteurs)))
