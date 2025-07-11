243625bcdaa3db91e548ed4e935c1dbb2700ee1c
David COBAC <david.cobac@ac-nantes.fr>
2025-07-11 10:39:08 +0200

    25-S13-1

---
+++ 25_sujet13_1.scm
@@ -0,0 +1,13 @@
+; david cobac
+; juillet 2025
+(define (recherche elt lst)
+  (let boucle ((n 0)
+	       (liste lst))
+    (cond ((null? liste) #f)
+	  ((= elt (car liste)) n)
+	  (else (boucle (+ 1 n) (cdr liste))))))
+
+; tests
+(let ((listes '((1 1 50 15)
+		((2 3 4) (10 12 1 56) (1 50 1) (8 9 10 15)))))
+  (newline)(write (apply map recherche listes)))
