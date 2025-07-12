1466562a20a501755f3006b4de94bb2aa849f63c
David COBAC <david.cobac@ac-nantes.fr>
2025-07-12 18:49:22 +0200

    25-S17-2

---
+++ 25_sujet17_2.scm
@@ -0,0 +1,11 @@
+; david cobac
+; juillet 2025
+(define (ajoute indice element lst)
+  (let boucle ((liste lst)
+	       (n 0))
+    (if (= indice n) (cons element liste)
+	(cons (car liste) (boucle (cdr liste) (+ 1 n))))))
+
+; tests
+(let ((liste '(1 3 0)))
+  (map (lambda (n) (ajoute n 4 '(7 8 9))) liste))
