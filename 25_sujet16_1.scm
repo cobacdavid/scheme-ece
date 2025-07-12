31ceb0defd26a4849b16cc7bac7596c141b2b4b9
David COBAC <david.cobac@ac-nantes.fr>
2025-07-12 16:16:57 +0200

    25-S16-1

---
+++ 25_sujet16_1.scm
@@ -0,0 +1,14 @@
+; david cobac
+; juillet 2025
+(define (moyenne lst)
+  (let boucle ((liste lst)
+	       (sprod 0)
+	       (somme-cdr 0))
+    (if (null? liste) (/ sprod somme-cdr)
+	(let* ((p (car liste))
+	       (note (car p))
+	       (coef (cdr p)))
+	  (boucle (cdr liste) (+ sprod (* note coef)) (+ somme-cdr coef))))))
+
+; tests
+(moyenne '((15. . 2) (9. . 1) (12. . 3)))
