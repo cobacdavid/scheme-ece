a50467fd91a4bb9d4777ee3a67bb55f91aac4e78
David COBAC <david.cobac@ac-nantes.fr>
2025-07-12 15:56:05 +0200

    25-S15-2

---
+++ 25_sujet15_2.scm
@@ -0,0 +1,18 @@
+; david cobac
+; juillet 2025
+(define (chercher vct x i j)
+  (if (> i j) #f
+      (let* ((m (floor (/ (+ i j) 2)))
+	     (elt (vector-ref vct m)))
+	(cond ((< elt x) (chercher vct x (+ 1 m) j))
+	      ((> elt x) (chercher vct x i (- m 1)))
+	      (else m)))))
+
+; tests
+(let* ((vecteur1 #(1 5 6 6 9 12))
+       (vecteur2 #(1))
+       (listes (list (list vecteur1 vecteur1 vecteur1 vecteur2 vecteur2)
+		     '(7 9 6 0 1)
+		     '(0 0 0 0 0)
+		     '(5 5 5 0 0))))
+  (newline)(write (apply map chercher listes)))
